7fa2873f5ef0975c83488fd30d182e3b789f4639
Damien Flury <damien.flury@gmail.com>
2018-11-10 15:32:54 +0100

    Draw board to console

--- src/Chess.App/Program.cs
+++ src/Chess.App/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace Chess.App
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DrawBoardToConsole();
+        }
+
+        private static void DrawBoardToConsole()
+        {
+            const int height = 42;
+            const int width = 122;
+
+            for (var y = 0; y <= height; y++)
+            {
+                for (var x = 0; x <= width; x++)
+                {
+                    Console.Write((x == 0 || x == width) && (y - 8) % 5 == 0 ? (char)(49 + y / 5) : (y == 0 || y == height) && (x - 8) % 15 == 0 ? (char)(65 + x / 15) : (x - 1) % (width / 8) == 0 ? '|' : (y - 1) % (height / 8) == 0 ? '–' : ' ');
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
