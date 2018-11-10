7b47155ecd9a53a812330f030e9833329281540a
Damien Flury <damien.flury@gmail.com>
2018-11-10 14:49:25 +0100

    Add projects

---
+++ src/Chess.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chess.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
